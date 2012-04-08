af6bdd1024733d2dc6a39e671406db68781ffe12
Dave Anderson <dave@electrobarn.com>
2012-04-08 10:58:23 -0600

    Initial commit

---
+++ pelican.conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Dave Anderson"
+SITENAME = u"skipandmadge.com"
+SITEURL = 'http://skipandmadge.com/'
+
+TIMEZONE = 'America/Edmonton'
+
+DEFAULT_LANG='en'
+
+GITHUB_URL = 'http://github.com/electrobarn/'
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+THEME = u'themes/electrobarn'
+
+# Blogroll
+# LINKS =  (
+#     ('Robots and Pencils', 'http://robotsandpencils.com'),
+#          )
+
+# Social widget
+SOCIAL = (
+          ('twitter', 'http://twitter.com/electrobarn'),
+          ('github', 'http://github.com/electrobarn'),
+         )
+
+DEFAULT_PAGINATION = False
+
+
+    
